6e6c5c1c0fd2f41f3c20ebdf3c75b518b6e453f4
abdurhmn.mhd <114473855+abdurhmnmhd@users.noreply.github.com>
2024-03-21 08:41:00 +0700

    Add files via upload

---
+++ PBO Modifier access/app.java
@@ -0,0 +1,8 @@
+public class app {
+    public static void main(String[] args) {
+    
+        warga warga = new warga("Abdurahman", 224356, "Laki laki");
+        System.out.println(warga);
+    }
+    
+}
